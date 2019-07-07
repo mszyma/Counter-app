955115189088f4efb49dcf36e7505c147b90e95c
Mariusz Szyma <m.szyma92@gmail.com>
2019-07-07 23:59:55 +0200

    raising and handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
 class Counter extends Component {
-  state = {
-    value: this.props.value
-  };
-
-  handleIncrement = () => {
-    console.log("Increment Clicked", this);
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
-    console.log("props", this.props);
     return (
       <div className={this.getDivClasses()}>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary">
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary"
+        >
           Increment
+        </button>
+
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
@@ -32,7 +32,7 @@ class Counter extends Component {
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
