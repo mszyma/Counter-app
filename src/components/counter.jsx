9e6674d703e4079bd4cfcc982e3bcaef96c60ab2
Mariusz Szyma <m.szyma92@gmail.com>
2019-07-07 16:48:19 +0200

    Added counters.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,38 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 1,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
+  };
+
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
+    console.log("props", this.props);
     return (
-      <React.Fragment>
+      <div className={this.getDivClasses()}>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-      </React.Fragment>
+        <button onClick={this.handleIncrement} className="btn btn-secondary">
+          Increment
+        </button>
+      </div>
     );
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  getDivClasses() {
+    let classes = "m-2";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
